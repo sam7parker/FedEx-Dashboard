55eb39bb789524b72e96644fac7001caad726533
Benny Jiang <103272373+benny-jiang@users.noreply.github.com>
2023-11-21 14:10:12 -0600

    Update login.js

---
+++ login.js
@@ -0,0 +1,25 @@
+function login() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var userType = document.getElementById("userType").value;
+
+  // Check if entered credentials are correct
+  if (email === "abc@gmail.com" && password === "12345") {
+    // Redirect user based on userType
+    switch (userType) {
+      case "courier":
+        window.location.href = "courier_dashboard.html";
+        break;
+      case "manager":
+        window.location.href = "manager_dashboard.html";
+        break;
+      case "executive":
+        window.location.href = "executive_dashboard.html";
+        break;
+      default:
+        alert("Invalid user type");
+    }
+  } else {
+    alert("Invalid credentials. Please try again.");
+  }
+}
