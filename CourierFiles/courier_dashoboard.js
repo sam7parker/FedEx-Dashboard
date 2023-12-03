20f37ed75e759390e67cde13ea61c76a65188a13
Benny Jiang <103272373+benny-jiang@users.noreply.github.com>
2023-12-03 14:55:54 -0600

    Update courier_dashoboard.js

--- CourierFiles/courier_dashoboard.js
+++ CourierFiles/courier_dashoboard.js
@@ -1,79 +1,48 @@
+// courier_dashboard.js
 // Function to update package information
 function updatePackageInfo() {
-    // Simulated package data (replace this with actual data)
-    const packageData = [
-        {
-            id: 'PKG123456',
-            status: 'In Transit',
-            lastLocation: 'Warehouse A',
-            deliveryETA: 'December 5, 2023',
-        },
-        {
-            id: 'PKG789012',
-            status: 'In Transit',
-            lastLocation: 'Warehouse B',
-            deliveryETA: 'December 7, 2023',
-        },
-        // Add more package data objects as needed
-        {
-            id: 'PKG782012',
-            status: 'In Transit',
-            lastLocation: 'Warehouse c',
-            deliveryETA: 'December 8, 2023',
-        },
-        {
-            id: 'PCR781132',
-            status: 'In Transit',
-            lastLocation: 'Warehouse D',
-            deliveryETA: 'December 9, 2023',
-        },
-    ];
+  // Simulated package data (replace this with actual data)
+  const packageData = {
+    id: "PKG123456",
+    status: "In Transit",
+    lastLocation: "Warehouse A",
+    deliveryETA: "December 5, 2023",
+  };
 
-    // Accessing the Package-info-content element
-    const packageInfoContent = document.querySelector('.Package-info-content');
+  // Accessing the Package-info-content element
+  const packageInfoContent = document.querySelector(".Package-info-content");
 
-    // Creating a container for package widgets
-    const widgetContainer = document.createElement('div');
-    widgetContainer.classList.add('widget-container');
+  // Creating HTML content with package information
+  const packageInfoHTML = `
+        <p>Package ID: ${packageData.id}</p>
+        <p>Status: 
+            <select class="status-dropdown">
+                <option value="In Transit">In Transit</option>
+                <option value="Delivered">Delivered</option>
+            </select>
+        </p>
+        <p>Last Location: ${packageData.lastLocation}</p>
+        <p>Estimated Delivery: ${packageData.deliveryETA}</p>
+    `;
 
-    packageData.forEach(package => {
-        // Creating a div for each package widget
-        const packageWidget = document.createElement('div');
-        packageWidget.classList.add('widget');
+  // Updating the content of Package-info-content with package information
+  packageInfoContent.innerHTML = packageInfoHTML;
 
-        // Generating HTML content for package details
-        packageWidget.innerHTML = `
-            <p>Package ID: ${package.id}</p>
-            <p>Status: ${package.status}</p>
-            <p>Last Location: ${package.lastLocation}</p>
-            <p>Estimated Delivery: ${package.deliveryETA}</p>
-        `;
+  // Get the dropdown element
+  const statusDropdown = document.querySelector(".status-dropdown");
 
-        // Append each package widget to the container
-        widgetContainer.appendChild(packageWidget);
-    });
+  // Set the initial selected value based on package data
+  statusDropdown.value = packageData.status;
 
-    // Append the container with package widgets to the Package-info-content
-    packageInfoContent.appendChild(widgetContainer);
-
-    // Set a fixed height for the package info content for scrolling
-    packageInfoContent.style.height = '400px';
-    packageInfoContent.style.overflowY = 'auto';
-
-    // Get all dropdown elements within package widgets
-    const statusDropdowns = document.querySelectorAll('.status-dropdown');
-
-    // Set initial selected values for each dropdown based on package data
-    statusDropdowns.forEach((dropdown, index) => {
-        dropdown.value = packageData[index].status;
-
-        dropdown.addEventListener('change', function () {
-            packageData[index].status = dropdown.value;
-            console.log("Selected status for package " + packageData[index].id + ": " + packageData[index].status);
-            // Additional actions or function calls related to status change can be added here
-        });
-    });
+  // Add event listener to update the status when the selection changes
+  statusDropdown.addEventListener("change", function () {
+    packageData.status = statusDropdown.value;
+    // Call a function to update the status or perform other actions based on the selected value
+    // For example: updateStatus(packageData.status);
+    // Replace updateStatus with the actual function to update status or perform necessary actions
+    console.log("Selected status: " + packageData.status); // Example: Log the selected status
+  });
 }
 
-// Call the function to update package information and set up the dropdowns
+// Call the function to update package information and set up the dropdown
 updatePackageInfo();
