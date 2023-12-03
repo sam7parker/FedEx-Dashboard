191ec994741ab9584da6219fc361bbd719d14b1f
Benny <benny.j0829@gmail.com>
2023-12-03 15:34:03 -0600

    added google map api

--- CourierFiles/courier_dashoboard.js
+++ CourierFiles/courier_dashoboard.js
@@ -1,47 +1,39 @@
 // courier_dashboard.js
-// Function to update package information
-function updatePackageInfo() {
+
+// Function to initialize the map
+function initMap() {
+  // Specify the initial map options (center, zoom, etc.)
+  var mapOptions = {
+    center: { lat: 37.7749, lng: -122.4194 }, // Example: San Francisco, CA
+    zoom: 10,
+  };
+
+  // Create the map object
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
   // Simulated package data (replace this with actual data)
   const packageData = {
     id: "PKG123456",
     status: "In Transit",
     lastLocation: "Warehouse A",
     deliveryETA: "December 5, 2023",
+    coordinates: { lat: 37.7749, lng: -122.4194 }, // Example: Coordinates for Warehouse A
   };
 
-  // Accessing the Package-info-content element
-  const packageInfoContent = document.querySelector(".Package-info-content");
-
-  // Creating HTML content with package information
-  const packageInfoHTML = `
-        <p>Package ID: ${packageData.id}</p>
-        <p>Status: 
-            <select class="status-dropdown">
-                <option value="In Transit">In Transit</option>
-                <option value="Delivered">Delivered</option>
-            </select>
-        </p>
-        <p>Last Location: ${packageData.lastLocation}</p>
-        <p>Estimated Delivery: ${packageData.deliveryETA}</p>
-    `;
-
-  // Updating the content of Package-info-content with package information
-  packageInfoContent.innerHTML = packageInfoHTML;
-
-  // Get the dropdown element
-  const statusDropdown = document.querySelector(".status-dropdown");
-
-  // Set the initial selected value based on package data
-  statusDropdown.value = packageData.status;
-
-  // Add event listener to update the status when the selection changes
-  statusDropdown.addEventListener("change", function () {
-    packageData.status = statusDropdown.value;
-    // Call a function to update the status or perform other actions based on the selected value
-    // For example: updateStatus(packageData.status);
-    // Replace updateStatus with the actual function to update status or perform necessary actions
-    console.log("Selected status: " + packageData.status); // Example: Log the selected status
+  // Add a marker for the last location of the package
+  var marker = new google.maps.Marker({
+    position: packageData.coordinates,
+    map: map,
+    title: `Last Location: ${packageData.lastLocation}`,
   });
+}
+
+// Call the function to initialize the map
+initMap();
+
+// Function to update package information
+function updatePackageInfo() {
+  // ... (Your existing package information update code)
 }
 
 // Call the function to update package information and set up the dropdown
