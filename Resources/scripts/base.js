e7f3d1d7f873132cfa6ac0d5ab2a90867d35e1b3
Brian Nguyen <briannguyen9101@gmail.com>
2023-12-05 21:45:29 -0600

    Added couriers outbound widget

--- Resources/scripts/base.js
+++ Resources/scripts/base.js
@@ -5,11 +5,9 @@ function HamburgerMenu() {
     if (menu.style.visibility === "hidden" && menu.style.left === "-200px") {
         menu.style.visibility = "visible"
         menu.style.left = "0"
-        overlay.style.visibility = "visible"
     }
     else {
         menu.style.visibility = "hidden"
         menu.style.left = "-200px"
-        overlay.style.visibility = "hidden"
     }
 }
