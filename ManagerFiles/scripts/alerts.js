2724d2efe810fd01ac79a3db3e825a6db1412198
Brian Nguyen <briannguyen9101@gmail.com>
2023-12-07 01:13:18 -0600

    Added message when there are no alerts for alerts page

--- ManagerFiles/scripts/alerts.js
+++ ManagerFiles/scripts/alerts.js
@@ -2,8 +2,14 @@ function ClearAllAlerts() {
     let alerts = document.querySelectorAll(".widget")
 
     if (alerts.length != 0) {
+        let page_header = document.getElementById("page-header")
+        let msg = document.createElement("h3")
+        msg.innerHTML = "No alerts..."
+
         for (alert of alerts) {
             alert.remove()
         }
+
+        page_header.insertAdjacentElement("afterend", msg)
     }
 }
