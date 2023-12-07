d341ee3e19303a4097dc4457ab4c36457be9b170
Brian Nguyen <briannguyen9101@gmail.com>
2023-12-06 19:51:18 -0600

    Improved layout of stats page for manager dashboard

--- ManagerFiles/scripts/statistics.js
+++ ManagerFiles/scripts/statistics.js
@@ -1,5 +1,5 @@
 // X-Axis for Line Graphs (Route and Scan)
-const xVals_line = [1,5,10,15,20,25,30]
+const xVals_line = [1, 5, 10, 15, 20, 25, 30]
 
 // Route Compliance Graph
 const RouteCompliance = new Chart("RouteCompliance", {
@@ -10,52 +10,54 @@ const RouteCompliance = new Chart("RouteCompliance", {
             label: "Route Compliance",
             backgroundColor: "rgba(255,102,0,0.5",
             borderColor: "rgba(77,20,140,1.0)",
-            data: [70,80,80,85,86,95,92],
+            data: [70, 80, 80, 85, 86, 95, 92],
         }]
     },
     options: {
         scales: {
-        yAxes: [{
-            display: true,
-            ticks: {
-                suggestedMin: 0,
-                suggestedMax: 100
-            }
-        }]
-    }}
-  });
+            yAxes: [{
+                display: true,
+                ticks: {
+                    suggestedMin: 0,
+                    suggestedMax: 100
+                }
+            }]
+        }
+    }
+});
 
 // Scan Compliance Graph
 const ScanCompliance = new Chart("ScanCompliance", {
-  type: "line",
-  data: {
-      labels: xVals_line,
-      datasets: [{
-          //backgroundColor: "rgba(77,20,140,1.0)",
-          //borderColor: "rgba(255,102,0,1.0",
-          label: "Route Compliance",
-          backgroundColor: "rgba(255,102,0,0.5",
-          borderColor: "rgba(77,20,140,1.0)",
-          data: [91,86,85,86,89,95,97],
-      }]
-  },
-  options: {
-      scales: {
-      yAxes: [{
-          display: true,
-          ticks: {
-              suggestedMin: 0,
-              suggestedMax: 100
-          }
-      }]
-  }}
+    type: "line",
+    data: {
+        labels: xVals_line,
+        datasets: [{
+            //backgroundColor: "rgba(77,20,140,1.0)",
+            //borderColor: "rgba(255,102,0,1.0",
+            label: "Route Compliance",
+            backgroundColor: "rgba(255,102,0,0.5",
+            borderColor: "rgba(77,20,140,1.0)",
+            data: [91, 86, 85, 86, 89, 95, 97],
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                display: true,
+                ticks: {
+                    suggestedMin: 0,
+                    suggestedMax: 100
+                }
+            }]
+        }
+    }
 });
 
-// Gross Revenue Graph
+// Package Pie Chart
 const GrossRevenue = new Chart("GrossRevenue", {
     type: "pie",
     data: {
-        labels:[
+        labels: [
             'Delivered',
             'Out for Delivery',
             'Exceptions'
@@ -78,12 +80,12 @@ const GrossRevenue = new Chart("GrossRevenue", {
 // Couriers Scheduled Graph
 const CouriersScheduled = new Chart("CouriersScheduled", {
     type: 'bar',
-    
+
     data: {
-        labels: [1,5,10,15,20,25,30],
+        labels: [1, 5, 10, 15, 20, 25, 30],
         datasets: [{
             label: "Couriers Scheduled",
-            data: [15,12,13,15,17,20,18],
+            data: [15, 12, 13, 15, 17, 20, 18],
             backgroundColor: "rgba(255,102,0,0.5)",
             bordercolor: "rgba(255,102,0)"
         }],
